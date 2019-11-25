0f2e033d6918949d5355081bcfbc3126e48d7ab4
Mohammad <3dt@live.com>
2019-11-25 17:21:10 +0200

    Added Scopes

--- app/src/main/java/com/mohammad/githubrepos/presentation/main/MainModule.kt
+++ app/src/main/java/com/mohammad/githubrepos/presentation/main/MainModule.kt
@@ -1,5 +1,6 @@
 package com.mohammad.githubrepos.presentation.main
 
+import com.mohammad.githubrepos.di.scopes.ActivityScope
 import com.mohammad.githubrepos.domain.IContentManager
 import com.mohammad.githubrepos.domain.ILogger
 import dagger.Binds
@@ -9,15 +10,18 @@ import dagger.Provides
 @Module
 abstract class MainModule {
 
+    @ActivityScope
     @Binds
     abstract fun bindView(activity: MainActivity):MainContract.View
 
     @Module
     companion object{
+        @ActivityScope
         @Provides
         @JvmStatic
         fun providePresenter(view: MainContract.View, interactor: MainContract.Interactor, logger: ILogger):MainContract.Presentor = MainPresenterImp(view, interactor, logger)
 
+        @ActivityScope
         @Provides
         @JvmStatic
         fun provideInteractor(contentManager: IContentManager):MainContract.Interactor = MainInteractor(contentManager)
