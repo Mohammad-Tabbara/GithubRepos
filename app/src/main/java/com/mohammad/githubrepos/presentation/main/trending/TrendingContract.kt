dfb0f9670100662d72dbc35fb9165efda57a8ff2
Mohammad <3dt@live.com>
2019-11-25 16:42:32 +0200

    Refactored Trending from MVP to MVVM

--- app/src/main/java/com/mohammad/githubrepos/presentation/main/trending/TrendingContract.kt
+++ app/src/main/java/com/mohammad/githubrepos/presentation/main/trending/TrendingContract.kt
@@ -5,21 +5,6 @@ import com.mohammad.githubrepos.framework.api.models.RepoWrapper
 import com.mohammad.githubrepos.presentation._common.rxjava.ApiListener
 
 class TrendingContract {
-    interface View {
-        fun initLayout(repos: MutableList<Repo>, trendingSpan: Int)
-        fun upDateData()
-        fun loadMoreFailed()
-        fun showLoading()
-        fun showData()
-        fun showNoNetwork()
-    }
-
-    interface Presentor {
-        fun onViewCreated()
-        fun loadMore(offset: Int)
-        fun retryClicked()
-    }
-
     interface Interactor {
         fun getTrendingRepositories(since: String, page: Int, apiListener: ApiListener<RepoWrapper>)
         fun getTrendingSpan(): Int
--- app/src/main/java/com/mohammad/githubrepos/presentation/main/trending/TrendingModule.kt
+++ app/src/main/java/com/mohammad/githubrepos/presentation/main/trending/TrendingModule.kt
@@ -1,22 +1,24 @@
 package com.mohammad.githubrepos.presentation.main.trending
 
+import androidx.lifecycle.ViewModel
+import com.mohammad.githubrepos.di.annotations.ViewModelKey
 import com.mohammad.githubrepos.domain.IContentManager
 import com.mohammad.githubrepos.domain.ILogger
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
+import dagger.multibindings.IntoMap
 
 @Module
 abstract class TrendingModule {
 
-    @Binds
-    abstract fun bindView(fragment: TrendingFragment):TrendingContract.View
-
     @Module
     companion object{
         @Provides
+        @IntoMap
+        @ViewModelKey(TrendingViewModel::class)
         @JvmStatic
-        fun providePresenter(view: TrendingContract.View, interactor: TrendingContract.Interactor, logger: ILogger):TrendingContract.Presentor = TrendingPresenterImp(view, interactor, logger)
+        fun provideViewModel(interactor: TrendingContract.Interactor, logger: ILogger): ViewModel = TrendingViewModel(interactor, logger)
 
         @Provides
         @JvmStatic
